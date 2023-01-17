68dbb1d3a2aa81d873076784b8982d994c6b592a
Natalia <natalia-9988@yandex.ru>
2023-01-17 18:27:51 +0500

    Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] GetArrayRandom (int length)
+{
+    int[] arr = new int [length];
+    for (int i=0; i< length; i++)
+    {
+        arr[i] = new Random().Next(0,100);
+    }
+    return arr;
+}
+int [] array = GetArrayRandom(8);
+
+void showArray ( int[]array)
+{
+    Console.Write($"[");
+    for (int i=0; i< array.Length; i++)
+    {
+        if( i != array.Length -1)
+        {
+            Console.Write($"{array[i]},");
+        }
+        else
+        {
+            Console.Write($"{array[i]}]");
+        
+        }
+    }
+}
+showArray(array);
+
+ 
